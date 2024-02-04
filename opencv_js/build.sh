0debaeba81c1c0feab2f7ce6d05f0ddf60006a64
Isabella Hogg <29220416+Isabella97H@users.noreply.github.com>
2024-02-04 22:57:57 +1000

    update build script and fix index.html

--- opencv_js/build.sh
+++ opencv_js/build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -e
-echo Cloning OpenCV
-git clone git@github.com:opencv/opencv.git
+echo Setup latest EMSDK
+git clone https://github.com/emscripten-core/emsdk.git || (cd emsdk && git pull)
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
+source "$(pwd)/emsdk_env.sh"
+cd ..
+
+echo Get latest OpenCV
+git clone https://github.com/opencv/opencv.git || (cd opencv && git pull)
+
+echo Copy in custom config
+\cp opencv_js/opencv_js.config.py opencv/platforms/js/
 
 echo Building opencv.js and opencv.wasm
 cd opencv
-docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk python3 ./platforms/js/build_js.py wasm_build --build_wasm --disable_single_file --cmake_option="-DBUILD_LIST=js,core,imgproc" --config=../opencv_js/opencv_js.config.py
+docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk python3 ./platforms/js/build_js.py wasm_build \
+--build_wasm --disable_single_file --cmake_option="-DBUILD_LIST=js,core,imgproc" \
+--emscripten_dir=../emsdk/upstream/emscripten
+sed -i "s/var Module=moduleArg;var readyPromiseResolve/var Module=moduleArg;var cv=Module;var readyPromiseResolve/1" opencv/wasm_build/bin/opencv.js
 
 echo Uploading artifacts to S3
-cd wasm_build/bin
-zip opencv.zip opencv.js opencv_js.wasm
-aws s3 cp opencv.zip s3://opencv-build-artifacts
+aws s3 cp --recursive opencv/wasm_build/bin s3://opencv-build-artifacts/
 
 echo Done
